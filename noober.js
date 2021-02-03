465a8bee3d142ef8b74d7b70af785b61ece11d46
JennaB10 <jennb1046@gmail.com>
2021-02-02 21:39:26 -0600

    final

--- noober.js
+++ noober.js
@@ -11,36 +11,32 @@ async function pageLoaded() {
 
   // 🔥 start here: write code to loop through the rides
 
-  
-  
-  
-//code to loop through the riders
+  //code to loop through the riders
 
 let renderTitle = function() {
 
   return `
 
-  <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
-  <i class="fas fa-car-side"></i>
-  <span>${levelOfService}</span>
-</h1>
-`
+      <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+      <i class="fas fa-car-side"></i>
+      <span>${levelOfService}</span>
+    </h1>
+  `
 }
 
-let renderRide = function(leg) {
+let renderPurple = function(leg) {
 
   return `
  
-
-    <div class="border-4 border-gray-900 p-4 my-4 text-left"> 
+    <div class="border-4 border-purple-500 p-4 my-4 text-left">
       <div class="flex">
         <div class="w-1/2">
           <h2 class="text-2xl py-1">${leg.passengerDetails.first} ${leg.passengerDetails.last}</h2>
           <p class="font-bold text-gray-600">${leg.passengerDetails.phoneNumber}</p>
         </div>
         <div class="w-1/2 text-right">
-          <span class="rounded-xl bg-gray-600 text-white p-2">
-            ${leg.numberOfPassengers} Passenger
+          <span class="rounded-xl bg-purple-600 text-white p-2">
+            ${leg.numberOfPassengers} passengers
           </span>
         </div>
       </div>
@@ -57,9 +53,40 @@ let renderRide = function(leg) {
         </div>
       </div>
     </div>
-  
    `
 
+ }
+
+let renderGray = function(leg) {
+
+  return `
+ 
+    <div class="border-4 border-gray-900 p-4 my-4 text-left"> 
+      <div class="flex">
+        <div class="w-1/2">
+          <h2 class="text-2xl py-1">${leg.passengerDetails.first} ${leg.passengerDetails.last}</h2>
+          <p class="font-bold text-gray-600">${leg.passengerDetails.phoneNumber}</p>
+        </div>
+        <div class="w-1/2 text-right">
+          <span class="rounded-xl bg-gray-600 text-white p-2">
+            ${leg.numberOfPassengers} passengers
+          </span>
+        </div>
+      </div>
+      <div class="mt-4 flex">
+        <div class="w-1/2">
+          <div class="text-sm font-bold text-gray-600">PICKUP</div>
+          <p>${leg.pickupLocation.address}</p>
+          <p>${leg.pickupLocation.city}, ${leg.pickupLocation.state} ${leg.pickupLocation.zip}</p>
+        </div>
+        <div class="w-1/2">
+          <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+          <p>${leg.dropoffLocation.address}</p>
+          <p>${leg.dropoffLocation.city}, ${leg.dropoffLocation.state} ${leg.dropoffLocation.zip}</p>
+        </div>
+      </div>
+    </div>
+   `
  }
 
 
@@ -68,12 +95,12 @@ for (let i = 0; i < rides.length; i++) {
    let element = document.querySelector('.rides') // let indicates your own variable
  
      // console.log(i)
-  //console.log(ride)
+    //console.log(ride)
 
-   // index.html is equivalent to document. "Document" represents the html page. Query Selector allows you to select pieces of html from a webpage  
-  // div, body, title are all html element
-  // look for any html element that has a class of CCS. Query selector needs a unique html 
-  // use html as template and javascript looks up data
+            // index.html is equivalent to document. "Document" represents the html page. Query Selector allows you to select pieces of html from a webpage  
+            // div, body, title are all html element
+            // look for any html element that has a class of CCS. Query selector needs a unique html 
+            // use html as template and javascript looks up data
  
   for(let ix=0; ix < ride.length; ix++){
    let leg = ride[ix]
@@ -103,26 +130,28 @@ for (let i = 0; i < rides.length; i++) {
 
   }
         // console.log(leg)
-   
-      //console.log(levelOfService)
+        //console.log(levelOfService)
+
 if (ix == 0) {
   
       element.insertAdjacentHTML('beforeend', renderTitle())}
 
-      element.insertAdjacentHTML('beforeend', renderRide(leg))
+if (leg.purpleRequested == true) {
+
+    element.insertAdjacentHTML('beforeend', renderPurple(leg))
+
+  }else{
+
+    element.insertAdjacentHTML('beforeend', renderGray(leg))
 
       }
 }
 
 //console log ride
 //console log leg 
-//
-
 // things you define in the loop have to be referenced in the loop 
 
-
-
-
+}
  }
 
 
